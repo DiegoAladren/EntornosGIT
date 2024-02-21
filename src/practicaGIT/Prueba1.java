58777a5e499e083ed8a392499b466698716bba18
Diego <diegoaladren854@gmail.com>
2024-02-21 19:32:36 +0100

    Primera versión de las clases Prueba1 y Prueb2 | versión 1.0

---
+++ src/practicaGIT/Prueba1.java
@@ -0,0 +1,8 @@
+package practicaGIT;
+
+public class Prueba1 {
+
+	public void inicial() {
+		System.out.println("Este es el primer metodo Prueba1");
+	}
+}
---
+++ src/practicaGIT/Prueba2.java
@@ -0,0 +1,8 @@
+package practicaGIT;
+
+public class Prueba2 {
+
+	public void inicial() {
+		System.out.println("Este es el primer metodo Prueba2");
+	}
+}
